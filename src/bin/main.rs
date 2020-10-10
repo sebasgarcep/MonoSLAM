6e55130c0db517456e4f5eb38d9acf6e08c81732
Sebastian Garrido <sebgar@gascaribe.com>
2020-10-10 12:45:06 -0500

    Read init JSON

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,38 +1,53 @@
 extern crate image;
+extern crate nalgebra;
 extern crate piston_window;
+extern crate serde_json;
+extern crate itertools;
 
-use piston_window::EventLoop;
+use nalgebra::{MatrixN, U3, U7, U13, VectorN};
+use serde_json::Value;
+use std::fs::File;
+use std::io::BufReader;
 
-const WIDTH: u32 = 320;
-const HEIGHT: u32 = 240;
+fn obj_to_vec(obj: &Value) -> Vec<f64> {
+    obj.as_array()
+        .unwrap()
+        .iter()
+        .map(|x| x.as_f64().unwrap())
+        .collect()
+}
+
+fn prop_to_vec(obj: &Value, prop: &str) -> Vec<f64> {
+    obj_to_vec(obj.get(prop).unwrap())
+}
 
 fn main() {
-    let mut window: piston_window::PistonWindow =
-        piston_window::WindowSettings::new("Raytracer", [WIDTH, HEIGHT])
-            .resizable(false)
-            .exit_on_esc(true)
-            .build()
-            .unwrap_or_else(|_e| { panic!("Could not create window!")});
+    let file = File::open("./data/init/mock.json").unwrap();
+    let reader = BufReader::new(file);
+    let init_json: Value = serde_json::from_reader(reader).unwrap();
 
-    window.set_max_fps(25);
+    let xv = VectorN::<f64, U13>::from_iterator(prop_to_vec(&init_json, "xv"));
+    println!("{:?}", xv);
 
-    let mut idx = 0;
-    while let Some(e) = window.next() {
-        let img = image::open(format!("./data/frames/rawoutput{:0>4}.pgm", idx))
+    for (idx, feature_object) in init_json.get("features").unwrap().as_array().unwrap().iter().enumerate() {
+        println!("{:?}", idx);
+        let yi = VectorN::<f64, U3>::from_iterator(prop_to_vec(feature_object, "yi"));
+        println!("{:?}", yi);
+        let xp = VectorN::<f64, U7>::from_iterator(prop_to_vec(feature_object, "xp_orig"));
+        println!("{:?}", xp);
+        let img = image::open(format!("./data/init/known_patch{}.pgm", idx))
             .unwrap()
             .to_rgba();
-
-        let tex = piston_window::Texture::from_image(
-            &mut window.create_texture_context(),
-            &img,
-            &piston_window::TextureSettings::new())
-            .unwrap();
-
-        window.draw_2d(&e, |c, g, _| {
-            piston_window::clear([1.0; 4], g);
-            piston_window::image(&tex, c.transform, g);
-        });
-
-        idx = (idx + 1) % 1000;
+        println!("{:?}", img.dimensions());
     }
+
+    let pxx = MatrixN::<f64, U13>::from_iterator(
+        itertools::concat(
+            init_json.get("pxx")
+                .unwrap()
+                .as_array()
+                .unwrap()
+                .iter()
+                .map(|a| obj_to_vec(a))));
+    println!("{:?}", pxx);
 }

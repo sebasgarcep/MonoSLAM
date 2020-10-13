38fae6b9abf89cf1769557dc4474e3b9b6fd5fd3
Sebastian Garrido <sebgar@gascaribe.com>
2020-10-13 00:24:06 -0500

    More work done on the Kalman Filter

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -15,7 +15,7 @@ use mono_slam::camera::WideAngleCamera;
 use mono_slam::constants::{BLOCKSIZE, NUM_FEATURES, MIN_DISTANCE_SQ, LINEAR_VELOCITY_NOISE, ANGULAR_VELOCITY_NOISE};
 use mono_slam::detection::Detection;
 use mono_slam::utils::{image_to_matrix, normalized_cross_correlation, unit_quaternion_from_angular_velocity};
-use nalgebra::{DMatrix, Matrix2, Matrix4, Matrix4x3, Matrix6, MatrixN, MatrixMN, U3, U6, U7, U13, Vector2, Vector3, VectorN, Quaternion, UnitQuaternion};
+use nalgebra::{DMatrix, DVector, Matrix2, Matrix4, Matrix4x3, Matrix6, MatrixN, MatrixMN, U3, U6, U7, U13, Vector2, Vector3, VectorN, Quaternion, UnitQuaternion};
 use serde_json::Value;
 use std::fs::File;
 use std::io::BufReader;
@@ -37,7 +37,7 @@ fn prop_to_vec(obj: &Value, prop: &str) -> Vec<f64> {
     obj_to_vec(obj.get(prop).unwrap())
 }
 
-fn get_dqwrnew_dqt(qwr: UnitQuaternion<f64>) -> Matrix4<f64> {
+fn get_dqwrnew_dqwr(qwr: UnitQuaternion<f64>) -> Matrix4<f64> {
     Matrix4::new(
         qwr[0], -qwr[1], -qwr[2], -qwr[3],
         qwr[1],  qwr[0],  qwr[3], -qwr[2],
@@ -46,7 +46,16 @@ fn get_dqwrnew_dqt(qwr: UnitQuaternion<f64>) -> Matrix4<f64> {
     )
 }
 
-fn get_dqt_domegar(wr: Vector3<f64>, delta_t: f64) -> Matrix4x3<f64> {
+fn get_dqwrnew_dqt(qwr: UnitQuaternion<f64>) -> Matrix4<f64> {
+    Matrix4::new(
+        qwr[0], -qwr[1], -qwr[2], -qwr[3],
+        qwr[1],  qwr[0], -qwr[3],  qwr[2],
+        qwr[2],  qwr[3],  qwr[0], -qwr[1],
+        qwr[3], -qwr[2],  qwr[1],  qwr[0],
+    )
+}
+
+fn get_dqt_dwr(wr: Vector3<f64>, delta_t: f64) -> Matrix4x3<f64> {
     let modulus = wr.norm();
     Matrix4x3::new(
         get_dq0_domega_a(wr[0], modulus, delta_t), get_dq0_domega_a(wr[1], modulus, delta_t), get_dq0_domega_a(wr[2], modulus, delta_t),
@@ -88,7 +97,8 @@ fn main() {
     let reader = BufReader::new(file);
     let init_json: Value = serde_json::from_reader(reader).unwrap();
 
-    let xv = VectorN::<f64, U13>::from_iterator(prop_to_vec(&init_json, "xv"));
+    // Initialize state
+    let xv = prop_to_vec(&init_json, "xv");
 
     let full_feature_vec: Vec<FullFeature> = init_json.get("features")
         .unwrap()
@@ -108,16 +118,31 @@ fn main() {
         })
         .collect();
 
-    let pxx = MatrixN::<f64, U13>::from_iterator(
-        itertools::concat(
-            init_json.get("pxx")
-                .unwrap()
-                .as_array()
-                .unwrap()
-                .iter()
-                .map(|a| obj_to_vec(a))
-        )
-    );
+    let pxx: Vec<_> =
+        init_json.get("pxx")
+            .unwrap()
+            .as_array()
+            .unwrap()
+            .iter()
+            .map(|a| obj_to_vec(a))
+            .collect();
+
+    let state_size = 13 + full_feature_vec.len() * 3;
+    let mut x_state = DVector::<f64>::zeros(state_size);
+    for i in 0..13 {
+        x_state[i] = xv[i];
+    }
+    for j in 0..full_feature_vec.len() {
+        for i in 0..3 {
+            x_state[13 + 3 * j + i] = full_feature_vec[j].yi[i];
+        }
+    }
+    let mut p_state = DMatrix::<f64>::zeros(state_size, state_size);
+    for i in 0..13 {
+        for j in 0..13 {
+            p_state[(i, j)] = pxx[i][j];
+        }
+    }
 
     // Get first frame and identify all features
     let camera = WideAngleCamera::new(
@@ -140,8 +165,8 @@ fn main() {
         .unwrap()
         .to_rgba();
 
-    let rw = xv.fixed_rows::<U3>(0);
-    let qwr = UnitQuaternion::from_quaternion(Quaternion::new(xv[3], xv[4], xv[5], xv[6]));
+    let rw = x_state.fixed_rows::<U3>(0);
+    let qwr = UnitQuaternion::from_quaternion(Quaternion::new(x_state[3], x_state[4], x_state[5], x_state[6]));
     let full_feature_rel_pos: Vec<_> = full_feature_vec
         .iter()
         .map(|feature| {
@@ -178,7 +203,7 @@ fn main() {
             let mut hri = camera.unproject(detection.pos);
             hri = hri / hri.norm();
             let hwi = qwr.transform_vector(&hri);
-            let xp_orig = xv.fixed_rows::<U7>(0).clone_owned();
+            let xp_orig = x_state.fixed_rows::<U7>(0).clone_owned();
             let patch_top_corner = ((detection.pos[0] as usize) - BLOCKSIZE / 2, (detection.pos[1] as usize) - BLOCKSIZE / 2);
             let patch = mat.slice(patch_top_corner, (BLOCKSIZE, BLOCKSIZE)).clone_owned();
             let particles = (0..100).map(|n| 0.5 + ((n as f64) / 100.0) * (5.0 - 0.5)).collect();
@@ -202,14 +227,21 @@ fn main() {
         let mat = image_to_matrix(&img);
 
         // Prediction step
-        let rw = xv.fixed_rows::<U3>(0);
-        let qwr = UnitQuaternion::from_quaternion(Quaternion::new(xv[3], xv[4], xv[5], xv[6]));
-        let vw = xv.fixed_rows::<U3>(7);
-        let wr = xv.fixed_rows::<U3>(10).clone_owned();
+        let rw = x_state.fixed_rows::<U3>(0);
+        let qwr = UnitQuaternion::from_quaternion(Quaternion::new(x_state[3], x_state[4], x_state[5], x_state[6]));
+        let vw = x_state.fixed_rows::<U3>(7);
+        let wr = x_state.fixed_rows::<U3>(10).clone_owned();
 
         // Assume linear and angular acceleration are 0. Predict next position and orientation.
         let rw_new = rw + vw * delta_t;
         let qwr_new = qwr * unit_quaternion_from_angular_velocity(wr * delta_t);
+        let mut fv = x_state.clone();
+        for i in 0..3 {
+            fv[i] = rw_new[i];
+        }
+        for i in 0..4 {
+            fv[3 + i] = qwr_new[i];
+        }
 
         // Calculate the covariance of the impulse vector.
         let pn = Matrix6::<f64>::new(
@@ -233,8 +265,8 @@ fn main() {
 
             dqwrnew_domegar = dqwrnew_dqt * dqt_domegar
         */
-        let dqwrnew_dqt = get_dqwrnew_dqt(qwr);
-        let dqt_domegar = get_dqt_domegar(wr, delta_t);
+        let dqwrnew_dqt = get_dqwrnew_dqt(qwr); // FIXME: double check this!
+        let dqt_domegar = get_dqt_dwr(wr, delta_t); // FIXME: double check this!
         let dqwrnew_domegar = dqwrnew_dqt * dqt_domegar;
         let dfv_dn = MatrixMN::<f64, U13, U6>::from_iterator(vec![
             delta_t, 0.0, 0.0, 0.0, 0.0, 0.0,
@@ -253,17 +285,93 @@ fn main() {
         ]);
 
         // Calculate the process noise covariance.
-        let qn = dfv_dn * pn * dfv_dn.transpose();
+        let qv = dfv_dn * pn * dfv_dn.transpose();
+
+        /*
+            The motion model maps it xv -> fv(xv) and yi -> yi. Therefore the jacobian of this model is:
+
+            df_dx = | dfv_dxv    0 |
+                    |       0    I |
+
+            The Jacobian of fv with respect to xv is:
+
+            dfv_dxv = | I    0               I * delta_t    0           |
+                      | 0    dqwrnew_dqwr    0              dqwrnew_dwr |
+                      | 0    0               I              0           |
+                      | 0    0               0              I           |
+
+            Let qt = q((wr + omegar) * delta_t). Then:
+
+            dqwrnew_dwr = dqwrnew_dqt * dqt_dwr
+
+            Same calculation as before.
+        */
+        let dqwrnew_dqwr = get_dqwrnew_dqwr(qwr);
+        let dqwrnew_dwr = dqwrnew_domegar;
+        let dfv_dxv = MatrixMN::<f64, U13, U13>::from_iterator(vec![
+            1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, delta_t, 0.0, 0.0, 0.0, 0.0, 0.0,
+            0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, delta_t, 0.0, 0.0, 0.0, 0.0,
+            0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, delta_t, 0.0, 0.0, 0.0,
+            0.0, 0.0, 0.0, dqwrnew_dqwr[(0, 0)], dqwrnew_dqwr[(0, 1)], dqwrnew_dqwr[(0, 2)], dqwrnew_dqwr[(0, 3)], 0.0, 0.0, 0.0, dqwrnew_dwr[(0, 0)], dqwrnew_dwr[(0, 1)], dqwrnew_dwr[(0, 2)],
+            0.0, 0.0, 0.0, dqwrnew_dqwr[(1, 0)], dqwrnew_dqwr[(1, 1)], dqwrnew_dqwr[(1, 2)], dqwrnew_dqwr[(1, 3)], 0.0, 0.0, 0.0, dqwrnew_dwr[(1, 0)], dqwrnew_dwr[(1, 1)], dqwrnew_dwr[(1, 2)],
+            0.0, 0.0, 0.0, dqwrnew_dqwr[(2, 0)], dqwrnew_dqwr[(2, 1)], dqwrnew_dqwr[(2, 2)], dqwrnew_dqwr[(2, 3)], 0.0, 0.0, 0.0, dqwrnew_dwr[(2, 0)], dqwrnew_dwr[(2, 1)], dqwrnew_dwr[(2, 2)],
+            0.0, 0.0, 0.0, dqwrnew_dqwr[(3, 0)], dqwrnew_dqwr[(3, 1)], dqwrnew_dqwr[(3, 2)], dqwrnew_dqwr[(3, 3)], 0.0, 0.0, 0.0, dqwrnew_dwr[(3, 0)], dqwrnew_dwr[(3, 1)], dqwrnew_dwr[(3, 2)],
+            0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+            0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0,
+            0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0,
+            0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0,
+            0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0,
+            0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0,
+        ]);
+
+        /*
+            The propagation of the covariance matrix is:
+
+            = df_dx * P * df_dx^T + Q
+            = | dfv_dxv    0 |   | Pxx Pxy |   | dfv_dxv^T    0 |
+              |       0    I | * | Pyx Pyy | * |       0      I | + Q
+            = | dfv_dxv * Pxx * dfv_dxv^T    dfv_dxv * Pxy |
+              | Pyx * dfv_dxv^T                        Pyy | + Q
+        */
+        // FIXME: missing Q
+        let mut p_state_new = DMatrix::<f64>::zeros(state_size, state_size);
+        // dfv_dxv * Pxx * dfv_dxv^T
+        let pxx_new = dfv_dxv * p_state.fixed_slice::<U13, U13>(0, 0) * dfv_dxv.transpose();
+        for i in 0..13 {
+            for j in 0..13 {
+                p_state_new[(i, j)] = pxx_new[(i, j)];
+            }
+        }
+        // dfv_dxv * Pxy
+        let pxy_new = dfv_dxv * p_state.slice((0, 13), (13, state_size - 13));
+        for i in 0..13 {
+            for j in 13..state_size {
+                p_state_new[(i, j)] = pxy_new[(i, j - 13)];
+                p_state_new[(j, i)] = pxy_new[(i, j - 13)];
+            }
+        }
+        // Pyy
+        for i in 13..state_size {
+            for j in 13..state_size {
+                p_state_new[(i, j)] = p_state[(i, j)];
+            }
+        }
 
         // Measurement step
-        for full_feature in &full_feature_vec {
-            let si = Matrix2::<f64>::identity() * camera.measurement_noise();
-            let size = 10; // FIXME: There should be a way to define how large the search area is.
-            let zi = camera.project(full_feature.yi - rw);
-            let x_min = max(BLOCKSIZE / 2, (zi[0].round() as usize) - size);
-            let x_max = min(camera.width() - BLOCKSIZE / 2, (zi[0].round() as usize) + size);
-            let y_min = max(BLOCKSIZE / 2, (zi[1].round() as usize) - size);
-            let y_max = min(camera.height() - BLOCKSIZE / 2, (zi[1].round() as usize) + size);
+        // Calculate innovation vector
+        let num_active_features = full_feature_vec.len();
+        let mut y_innov = DVector::<f64>::zeros(num_active_features * 2);
+        for i in 0..num_active_features {
+            let ref full_feature = full_feature_vec[i];
+            let size = 10;
+            let h_xi = camera.project(full_feature.yi - rw);
+            let h_xi_x = h_xi[0].round() as usize;
+            let h_xi_y = h_xi[1].round() as usize;
+            // Find best x, y
+            let x_min = max(BLOCKSIZE / 2, h_xi_x - size);
+            let x_max = min(camera.width() - BLOCKSIZE / 2, h_xi_x + size);
+            let y_min = max(BLOCKSIZE / 2, h_xi_y - size);
+            let y_max = min(camera.height() - BLOCKSIZE / 2, h_xi_y + size);
             let (mut best_x, mut best_y) = (0, 0);
             let mut best_corr = f64::NEG_INFINITY;
             for x in x_min..x_max {
@@ -277,15 +385,18 @@ fn main() {
                     }
                 }
             }
+            // Set the innovation vector = measurements - camera projection of current state of features
+            y_innov[2 * i] = (best_x - h_xi_x) as f64;
+            y_innov[2 * i + 1] = (best_y - h_xi_y) as f64;
         }
 
+        /*
         for partial_feature in &partial_feature_vec {
-            /*
             let particle_mean = stats::mean(partial_feature.particles.clone().into_iter());
             let particle_std = stats::stddev(partial_feature.particles.clone().into_iter());
             println!("{:?} {:?}", particle_mean, particle_std);
-            */
         }
+        */
     }
 
     /*
